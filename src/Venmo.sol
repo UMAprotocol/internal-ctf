f659b99e54101d411a7d18531c15a54520af3049
Matt Rice <matthewcrice32@gmail.com>
2023-12-06 08:17:42 -0600

    WIP
    Signed-off-by: Matt Rice <matthewcrice32@gmail.com>

--- src/Venmo.sol
+++ src/Venmo.sol
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.21;
 
 import {IERC20} from "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
@@ -35,42 +36,75 @@ contract Venmo {
         _send(msg.sender, to, amount, callback);
     }
 
-    function depositBySig(address user, uint256 amount, bytes calldata signature, Callback callback) external {
-        bytes32 hashedInfo = keccak256(abi.encode("Venmo::depositBySig", amount));
+    function depositBySig(
+        address user,
+        uint256 amount,
+        bytes calldata signature,
+        Callback callback
+    ) external {
+        bytes32 hashedInfo = keccak256(
+            abi.encode("Venmo::depositBySig", amount)
+        );
         require(ECDSA.recover(hashedInfo, signature) == user);
 
         _deposit(user, amount, callback);
     }
 
-    function withdrawBySig(address user, uint256 amount, bytes calldata signature, Callback callback) external {
-        bytes32 hashedInfo = keccak256(abi.encode("Venmo::withdrawBySig", amount));
+    function withdrawBySig(
+        address user,
+        uint256 amount,
+        bytes calldata signature,
+        Callback callback
+    ) external {
+        bytes32 hashedInfo = keccak256(
+            abi.encode("Venmo::withdrawBySig", amount)
+        );
         require(ECDSA.recover(hashedInfo, signature) == user);
 
         _withdraw(user, amount, callback);
     }
 
-    function sendBySig(address from, address to, uint256 amount, bytes calldata signature, Callback callback)
-        external
-    {
-        bytes32 hashedInfo = keccak256(abi.encode("Venmo::sendBySig", to, amount));
+    function sendBySig(
+        address from,
+        address to,
+        uint256 amount,
+        bytes calldata signature,
+        Callback callback
+    ) external {
+        bytes32 hashedInfo = keccak256(
+            abi.encode("Venmo::sendBySig", to, amount)
+        );
         require(ECDSA.recover(hashedInfo, signature) == from);
 
         _send(from, to, amount, callback);
     }
 
-    function _deposit(address user, uint256 amount, Callback callback) internal {
+    function _deposit(
+        address user,
+        uint256 amount,
+        Callback callback
+    ) internal {
         token.safeTransferFrom(user, address(this), amount);
         balances[user] += amount;
         callback.depositCallback(user, amount);
     }
 
-    function _withdraw(address user, uint256 amount, Callback callback) internal {
+    function _withdraw(
+        address user,
+        uint256 amount,
+        Callback callback
+    ) internal {
         token.safeTransfer(user, amount);
         balances[user] -= amount;
         callback.withdrawCallback(user, amount);
     }
 
-    function _send(address from, address to, uint256 amount, Callback callback) internal {
+    function _send(
+        address from,
+        address to,
+        uint256 amount,
+        Callback callback
+    ) internal {
         token.safeTransfer(to, amount);
         balances[from] -= amount;
         callback.sendCallback(from, to, amount);
